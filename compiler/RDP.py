1ead7566ee8f80a262c5e26d474de5878cb0bc7d
Shyam-Makwana <makwanashyam6@gmail.com>
2021-12-11 21:08:47 +0530

    Added different functionality of Complier

---
+++ compiler/RDP.py
@@ -0,0 +1,73 @@
+'''
+
+    The following program is a Python code for recursive descent parser. 
+    @author="Shyam Makwana"
+
+'''
+
+# 1 input and output box but one fixed grammar output
+
+print("\nRecursive Desent Parsing For following grammar\n")
+print("E->TE'\nE'->+TE'|^\nT->FT'\nT'->*FT'|^\nF->(E)|a\n")
+global inp
+inp = (input("Enter the string want to be checked : "))
+global i
+i=0
+global flag
+flag=0
+
+def match(ch):
+    global inp
+    global i
+    if(i>=len(inp)):
+        return False
+    elif(inp[i]==ch):
+        return True
+    else:
+        return False
+
+def F():
+    global inp
+    global i
+    global flag
+
+    if match('a'):
+        i = i + 1
+    elif match('('):
+        i = i + 1
+        E()
+        if match(')'):
+            i = i + 1
+        else:
+            flag=1
+    else:
+        flag=1
+
+def Tdash():
+    global i
+    if match('*'):
+        i+=1
+        F()
+        Tdash()
+
+def T():
+    F()
+    Tdash()
+
+def Edash():
+    global i
+    if match('+'):
+        i+=1
+        T()
+        Edash()
+
+def E():
+    T()
+    Edash()
+
+E()
+if len(inp)==i and flag==0:
+    print("\n",inp,"is Valid String")
+else:
+    print("\n",inp,"is Invalid String")
+
--- index.js
+++ index.js
@@ -17,17 +17,136 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/" + "index.html");
 });
 
-app.post("/api", async function (req, res) {
-  console.log("Starting......");
+app.get("/compiler", function (req, res) {
+  res.sendFile(__dirname + "/public/" + "compiler.html");
+});
+
+app.get("/firstandfollow", function (req, res) {
+  res.sendFile(__dirname + "/public/" + "fnf.html");
+});
+
+app.get("/leftfactoring", function (req, res) {
+  res.sendFile(__dirname + "/public/" + "lf.html");
+});
+
+app.get("/leftrecursion", function (req, res) {
+  res.sendFile(__dirname + "/public/" + "lr.html");
+});
+
+app.get("/token", function (req, res) {
+  res.sendFile(__dirname + "/public/" + "token.html");
+});
+
+app.post("/api/compiler", async function (req, res) {
+  // console.log("Starting......");
   var input = req.body.input;
-  console.log(input);
+  // console.log(input);
   input = input.concat("\n");
   fsPromises.writeFile("in.txt", input, function (err) {
     if (err) throw err;
     console.log("error to save " + filename);
   });
-  // var command = "a.out < in.txt";
+  // var command = "/a.out < in.txt";
   var command = "chmod 777 ./a.out && ./a.out < in.txt";
+  var x = {};
+  await exec(command, (error, stdout, stderr) => {
+    console.log( "From the execute function");
+    x = stdout;
+    console.log(stdout);
+    console.log('StdError = ',stderr);
+    console.log('Error = ',error);
+    res.send(x);
+    // console.log(r);
+  });
+  // res.send(x);
+});
+
+app.post("/api/fnf", async function (req, res) {
+  // console.log("Starting......");
+  var input = req.body.input;
+  // console.log(input);
+  input = input.concat("\n");
+  fsPromises.writeFile("in.txt", input, function (err) {
+    if (err) throw err;
+    console.log("error to save " + filename);
+  });
+  // var command = "/a.out < in.txt";
+  // var command = "chmod 777 ./a.out && ./a.out < in.txt";
+  var command = "python ./compiler/firstandfollow.py < in.txt";
+  var x = {};
+  await exec(command, (error, stdout, stderr) => {
+    // console.log( "From the execute function");
+    x = stdout;
+    // console.log(stdout);
+    // console.log('StdError = ',stderr);
+    // console.log('Error = ',error);
+    res.send(x);
+    // console.log(r);
+  });
+  // res.send(x);
+});
+
+app.post("/api/lf", async function (req, res) {
+  // console.log("Starting......");
+  var input = req.body.input;
+  // console.log(input);
+  input = input.concat("\n");
+  fsPromises.writeFile("in.txt", input, function (err) {
+    if (err) throw err;
+    console.log("error to save " + filename);
+  });
+  // var command = "/a.out < in.txt";
+  // var command = "chmod 777 ./a.out && ./a.out < in.txt";
+  var command = "python ./compiler/LeftFactoring.py < in.txt";
+  var x = {};
+  await exec(command, (error, stdout, stderr) => {
+    // console.log( "From the execute function");
+    x = stdout;
+    // console.log(stdout);
+    // console.log('StdError = ',stderr);
+    // console.log('Error = ',error);
+    res.send(x);
+    // console.log(r);
+  });
+  // res.send(x);
+});
+
+app.post("/api/lr", async function (req, res) {
+  // console.log("Starting......");
+  var input = req.body.input;
+  // console.log(input);
+  input = input.concat("\n");
+  fsPromises.writeFile("in.txt", input, function (err) {
+    if (err) throw err;
+    console.log("error to save " + filename);
+  });
+  // var command = "/a.out < in.txt";
+  // var command = "chmod 777 ./a.out && ./a.out < in.txt";
+  var command = "python ./compiler/LeftRecursion.py < in.txt";
+  var x = {};
+  await exec(command, (error, stdout, stderr) => {
+    // console.log( "From the execute function");
+    x = stdout;
+    // console.log(stdout);
+    // console.log('StdError = ',stderr);
+    // console.log('Error = ',error);
+    res.send(x);
+    // console.log(r);
+  });
+  // res.send(x);
+});
+
+app.post("/api/token", async function (req, res) {
+  // console.log("Starting......");
+  var input = req.body.input;
+  // console.log(input);
+  input = input.concat("\n");
+  fsPromises.writeFile("in.txt", input, function (err) {
+    if (err) throw err;
+    console.log("error to save " + filename);
+  });
+  // var command = "/a.out < in.txt";
+  var command = "chmod 777 ./token && ./token < in.txt";
   var x = {};
   await exec(command, (error, stdout, stderr) => {
     console.log( "From the execute function");
